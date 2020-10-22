cc81a2f55d3dc8eac1aae06d347394bb2fe3034c
Adam Scott <adam@launchxd.com>
2020-10-21 20:26:27 -0400

    initial commit

--- scripts/generate-images.js
+++ scripts/generate-images.js
@@ -1,74 +1,43 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
-import ffprobe from 'ffprobe';
-import ffprobeStatic from 'ffprobe-static';
-import ffmpeg from "ffmpeg";
+;
+import ffmpeg from 'ffmpeg';
 
 export async function getScreenShots(context) {
+  let files = undefined;
 
+  try {
+    let process = new ffmpeg('video/desktop-1.mp4');
+    console.log('loading ffmpeg');
+    process.then(
+      function (video) {
+        // Callback mode
+        console.log('loaded ffmpeg');
+        video.fnExtractFrameToJPG(
+          'public/frames/desktop',
+          {
+            every_n_seconds: 0.1,
+            file_name: 'example',
+          },
+          function (error, files) {
+            console.log('error', error);
 
-    let files = undefined;
+            if (!error) {
+              console.log('Frames: ' + files);
+            }
 
-    try {
-
-        let process = new ffmpeg('video/desktop-1.mp4');
-        console.log('loading ffmpeg');
-        process.then(function (video) {
-            // Callback mode
-            console.log('loaded ffmpeg');
-            video.fnExtractFrameToJPG('public/frames/desktop', {
-                every_n_seconds : 0.1,
-                file_name : 'example'
-            }, function (error, files) {
-
-                console.log('error', error);
-
-                if (!error) {
-                    console.log('Frames: ' + files);
-                }
-
-                return {
-                    props: {
-                        files
-                    },
-                };
-
-            });
-        }, function (err) {
-            console.log('Error: ' + err);
-        });
-    } catch (e) {
-        console.log(e.code);
-        console.log(e.msg);
-    }
-
-
-    return {
-        props: {
-            duration
-        },
-    };
-}
-
-export default function Home({duration}) {
-    return (
-        <div className={styles.container}>
-            <Head>
-                <title>Create Next App</title>
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-
-            <main className={styles.main}>
-                <h1 className={styles.title}>
-                    Welcome to <a href="https://nextjs.org">Next.js!</a>
-                </h1>
-
-                <p className={styles.description}>
-                    Generating images
-                    <pre>
-            {JSON.stringify(duration, null, 2)}
-          </pre>
-                </p>
-        </div>
-);
+            return {
+              props: {
+                files,
+              },
+            };
+          }
+        );
+      },
+      function (err) {
+        console.log('Error: ' + err);
+      }
+    );
+  } catch (e) {
+    console.log(e.code);
+    console.log(e.msg);
+  }
 }
