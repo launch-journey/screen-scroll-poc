9bac6d8195968227762d6b178e2c9d7e627cd8d3
Adam Scott <adam@launchxd.com>
2020-10-21 20:25:27 -0400

    initial checkin

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,72 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import {useEffect} from "react";
 
-export default function Home() {
+export async function getStaticProps(context) {
+
+  return {
+    props: {
+      frames: 369
+    },
+  };
+}
+
+export default function Home({frames}) {
+
+
+
+  useEffect(()=>{
+
+      const html = document.documentElement;
+      const canvas = document.getElementById("example");
+      const context = canvas.getContext("2d");
+
+      const frameCount = frames;
+      const currentFrame = index => (
+          `/frames/example_${index.toString()}.jpg`
+      )
+
+      const preloadImages = () => {
+          for (let i = 1; i < frameCount; i++) {
+              const img = new Image();
+              img.src = currentFrame(i);
+          }
+      };
+
+      const img = new Image()
+      img.src = currentFrame(1);
+      canvas.width=2400;
+      canvas.height=1500;
+      context.fillStyle = "white";
+      context.fillRect(0, 0, canvas.width, canvas.height);
+
+      img.onload=function(){
+          context.drawImage(img, 0, 0);
+      }
+
+      const updateImage = index => {
+          img.src = currentFrame(index);
+          context.drawImage(img, 0, 0);
+      }
+
+      window.addEventListener('scroll', () => {
+          const scrollTop = html.scrollTop;
+          const maxScrollTop = html.scrollHeight - window.innerHeight;
+          const scrollFraction = scrollTop / maxScrollTop;
+          const frameIndex = Math.min(
+              frameCount - 1,
+              Math.ceil(scrollFraction * frameCount)
+          );
+
+          requestAnimationFrame(() => updateImage(frameIndex + 1))
+      });
+
+      preloadImages()
+
+
+
+  },[])
+
   return (
     <div className={styles.container}>
       <Head>
@@ -10,56 +75,12 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+        <canvas id="example" className={styles.canvas} style={{width: "100vw", height:"100vh"}}>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+        </canvas>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
-  )
+  );
 }
